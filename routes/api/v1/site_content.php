134f5e49132c162104521e8b20520b777bb7a231
jkmdev <jkm96.dev@gmail.com>
2024-08-16 15:48:03 +0300

    fix: use the correct endpoint to fetch site content

--- routes/api/v1/site_content.php
+++ routes/api/v1/site_content.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Content\ContentMgmtController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1/site-content', 'namespace' => 'api/v1', 'middleware' => 'api'], function () {
-    Route::get('', [ContentMgmtController::class, 'getSiteContent']);
+    Route::get('list', [ContentMgmtController::class, 'getSiteContent']);
     Route::post('customer-feedback', [ContentMgmtController::class, 'saveCustomerFeedback']);
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('create', [ContentMgmtController::class, 'createContent']);
